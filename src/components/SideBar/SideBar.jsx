27c5b758279f5ba18582354b91e35cec5a7bff33
Samuel Paulo <112398583+MuelTec@users.noreply.github.com>
2023-07-17 11:34:55 -0300

    botão de click sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Logo from '../Logo/Logo';
+import React, { useState } from 'react';
+import logo from '../../imagens/logo.jpg';
 import Search from '../Search/Search';
 import Home from '../Home/Home';
 import Setup from '../Setup/Setup';
@@ -9,11 +9,25 @@ import Favoritos from '../Favoritos/Favoritos';
 import Bottom from '../Bottom/Bottom';
 
 const SideBar = () => {
+  const [style, setStyle] = useState('false');
+
+  const hancleclick = () =>{setStyle(!style);};
 
   return (
-    <nav className="sidebar">
-      <Logo/>
-      {/* sidebar close */}
+    <nav className={style ? 'sidebar' : 'sidebar close'}>
+      <header>
+        <div className="image-text">
+          <span className="image">
+            <img src={logo} alt="logo"/>
+          </span>
+
+          <div className="text header-text">
+            <span className="name">MuelTec</span>
+            <span>Loja de Informatíca</span>
+          </div>
+        </div>
+        <i className="bx bx-chevron-right toggle" onClick={hancleclick}></i>
+      </header>
       <div className="menu-bar">
         <div className="menu">
           <Search />
